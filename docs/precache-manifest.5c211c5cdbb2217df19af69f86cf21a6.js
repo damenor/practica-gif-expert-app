76e1ed3ed25ac29b25b54dd3b59f3034fc30ee9d
damenor <damenor.develop@gmail.com>
2020-08-02 18:37:14 +0200

    Add docs for github pages

---
+++ docs/precache-manifest.5c211c5cdbb2217df19af69f86cf21a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941f8804791fec847f91aa557f120dce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3319a9d8a5fd3706750b",
+    "url": "/static/css/main.ce77491f.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3319a9d8a5fd3706750b",
+    "url": "/static/js/main.d7cdaeed.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
